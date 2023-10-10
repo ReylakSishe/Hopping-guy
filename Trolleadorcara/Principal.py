9c549280c6ceec82269e6062b15a111749377df9
ReylakSishe <145812190+ReylakSishe@users.noreply.github.com>
2023-10-10 13:22:16 -0300

    a

--- Trolleadorcara/Principal.py
+++ Trolleadorcara/Principal.py
@@ -1,5 +1,6 @@
 import pyglet
 from pyglet.window import key
+from pyglet.window import mouse
 
 window = pyglet.window.Window()
 
@@ -13,6 +14,15 @@ def on_key_press(symbol, modifiers):
         print('La tecla "Abajo" ha sido presionada.')
     elif symbol == key.RIGHT:
         print('La tecla "Derecha" ha sido presionada')
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    if button == mouse.LEFT:
+        print("usted ha usado el click izquierdo")
+    elif button == mouse.RIGHT:
+        print("usted ha usado el click derecho")
+    elif button == mouse.MIDDLE:
+        print("usted ha usado el click medio")
 
 @window.event
 def on_draw():
--- Trolleadorcara/Prueba.py
+++ Trolleadorcara/Prueba.py
@@ -1,21 +1,15 @@
 import pyglet
-from pyglet.window import key
+from pyglet.window import mouse
 
 window = pyglet.window.Window()
 
 @window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.UP:
-        print('La tecla "Arriba" ha sido presionada.')
-    elif symbol == key.LEFT:
-        print('La tecla "Izquierda" ha sido presionada.')
-    elif symbol == key.DOWN:
-        print('La tecla "Abajo" ha sido presionada.')
-    elif symbol == key.RIGHT:
-        print('La tecla "Derecha" ha sido presionada')
-
-@window.event
-def on_draw():
-    window.clear()
+def on_mouse_press(x, y, button, modifiers):
+    if button == mouse.LEFT:
+        print("usted ha usado el click izquierdo")
+    elif button == mouse.RIGHT:
+        print("usted ha usado el click derecho")
+    elif button == mouse.MIDDLE:
+        print("usted ha usado el click medio")
 
 pyglet.app.run()
